0d03b50df71514519f3f9a7ba4b330dba8b8db71
Eric Holk <eric.holk@gmail.com>
2021-01-19 22:23:31 -0800

    Fix text display bug
    There were two problems:
    
    1. I wrote `mov ax, es` instead of `mov es, ax`.
    2. Loading from memory doesn't update flags, so the `mov al, [si]` line needed
       to be followed by `test al, al`
    
    Unfortunately, adding the `test al, al` instructions put us over the 510 byte
    limit for a boot sector, so I made the game slightly less exciting by removing
    the exclamation marks at the end of the strings.

--- boot.asm
+++ boot.asm
@@ -201,8 +201,8 @@ add [ball_y], bx
 
 jmp main_loop
 
-player_1_message: db 'Player 1 scores!', 0
-player_2_message: db 'Player 2 scores!', 0
+player_1_message: db 'Player 1 scores', 0
+player_2_message: db 'Player 2 scores', 0
 
 player_1_point:
 mov si, player_1_message
@@ -217,15 +217,15 @@ mov al, 3
 int 10h
 
 mov ax, 0xB800
-mov ax, es
+mov es, ax
 
 mov di, 12 * 80
 
 .string_loop:
 mov al, [si]
-mov ah, 15
+test al, al
 jz .exit_loop
-mov [es:di], ax
+mov [es:di], al
 add di, 2
 add si, 1
 jmp .string_loop
@@ -236,6 +236,7 @@ mov di, 14 * 80
 
 .string_loop2:
 mov al, [si]
+test al, al
 jz .exit_loop2
 mov [es:di], al
 add di, 2
