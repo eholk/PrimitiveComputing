5235212e563ffc533fc5ec0a6e906122da38f5f9
Eric Holk <eric.holk@gmail.com>
2021-03-07 21:26:05 -0800

    Episode 6 - Strings and testing
    Laying the foundation for a more advanced programming language. The first thing
    to do decide on a string representation, and some other basic data types.
    
    Second, we add a super simple unit testing framework, where we can define a
    list of test cases and the computer automatically runs through them all at
    boot. This will make it easier to add more advanced features in the future,
    since we can make sure nothing that was already working breaks.

--- boot.asm
+++ boot.asm
@@ -52,26 +52,91 @@ SECTION .main vstart=0x8000
 
 jmp beginning
 
-test_token1: db '1', 0             ;; 1
-test_token2: db '42', 0            ;; 42
-test_token3: db '     123', 0      ;; 123
-test_case1: db '+ 1 2', 0          ;; 3
-test_case2: db '* 2 3', 0          ;; 6
-test_case3: db '+ 12 34', 0        ;; 46
-test_case4: db '* + 1 2 + 3 4', 0  ;; 21
+test_token1: db '1', 0          ;; 1
+test_token1_len: equ $ - test_token1
+test_token2: db '42'            ;; 42
+test_token2_len: equ $ - test_token2
+test_token3: db '     123'      ;; 123
+test_token3_len: equ $ - test_token3
+test_case1: db '+ 1 2'          ;; 3
+test_case1_len: equ $ - test_case1
+test_case2: db '* 2 3'          ;; 6
+test_case2_len: equ $ - test_case2
+test_case3: db '+ 12 34'        ;; 46
+test_case3_len: equ $ - test_case3
+test_case4: db '* + 1 2 + 3 4'  ;; 21
+test_case4_len: equ $ - test_case4
+
+;; String representation (fat pointers)
+;;
+;; (16 bits: pointer to buffer, 16 bits: length)
+
+;; Test case representation
+;;
+;; (input expression as string, expected value as 16 bit int)
+all_tests:
+dw test_token1, test_token1_len, 1
+dw test_token2, test_token2_len, 42
+dw test_token3, test_token3_len, 123
+dw test_case1, test_case1_len, 3
+dw test_case2, test_case2_len, 6
+dw test_case3, test_case3_len, 46
+dw test_case4, test_case4_len, 21
+dw 0 ;; end
 
 beginning:
-mov ax, cs
-mov ds, ax
 
-mov ax, test_case4
+;; for each test case case:
+;;   print test case
+;;   evaluate
+;;   print if successful
+
+mov di, all_tests
+mov ax, ds 
+mov es, ax
+
+test_loop:
+mov bp, [di]
+test bp, bp
+jz empty_loop
+
+mov ax, begin_test_string
+call print_string
+mov ax, di
+call print_string
+mov ax, end_test_string
+call print_string
+
+
+mov ax, [di]
 call set_token_buffer
 call compute
 call print_number
+cmp ax, [di+4] ;; compare result with expected answer
+je .continue
 
-main_loop:
-jmp main_loop
+mov ax, failure_string
+call print_string
+
+;; advance to next test case
+.continue:
+add di, 6
+jmp test_loop
+
+begin_test_string_data: db 10, 13, 'Test: `'
+begin_Test_string_len: equ $ - begin_test_string_data
+begin_test_string: dw begin_test_string_data, begin_Test_string_len
+end_test_string_data: db '` => '
+end_test_string_len: equ $ - end_test_string_data
+end_test_string: dw end_test_string_data, end_test_string_len        
+failure_string_data: db 10, 13, '  FAIL'
+failure_string_len: equ $ - failure_string_data
+failure_string: dw failure_string_data, failure_string_len
+
+empty_loop:
+jmp empty_loop
 
 %include "print_number.asm"
 %include "get_token.asm"
 %include "compute.asm"
+%include "print_string.asm"
